54c4716e586b31cbde7005fa26fafa19af39d834
Abhishek Wasave <Abhi_wasave@abw12>
2023-02-06 12:16:48 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+import useMediaQuery from "./hooks/useMediaQuery";
+import Navbar from "./scenes/Navbar";
+import DotGroup from "./scenes/DotGroup";
+import Landing from "./scenes/Landing";
+
+function App() {
+  const [selectedPage, setSelectedPage] = useState("home"); //this state will determine we are at which page , initialpage is passed as home
+  const [isTopOfPage, setIsTopOfPage] = useState(true);
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
+
+  //useEffect basically runs when the component loads
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) setIsTopOfPage(true);
+      if (window.scrollY !== 0) setIsTopOfPage(false);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <div className="app bg-deep-blue">
+      <Navbar
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
+      <div className="w-5/6 mx-auto md:h-full">
+        {isAboveMediumScreens && (
+          <DotGroup
+            selectedPage={selectedPage}
+            setSelectedPage={setSelectedPage}
+          />
+        )}
+        <Landing setSelectedPage={setSelectedPage} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
